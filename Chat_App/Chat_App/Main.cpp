43e2eaf9a32519d3812cc3726d4bebe0a105bf1e
Amirali Mohayaee <amig90@yahoo.com>
2020-06-04 20:12:20 +0100

    First working server code

--- Chat_App/Chat_App/Main.cpp
+++ Chat_App/Chat_App/Main.cpp
@@ -4,7 +4,13 @@
 #include <SDL_net.h>
 
 
+/////////////////////////////////////
+//	Server-side code
+/////////////////////////////////////
+
+
 std::string message;
+
 
 IPaddress ip;
 
@@ -42,66 +48,68 @@ void Shutdown()
 
 int main(int argc, char* argv[])
 {
-	Initialize();
 
-	// 
-	if (SDLNet_ResolveHost(&ip, "www.google.com", 80) == -1)
+	if (!Initialize())
 	{
-		std::cout << "Failed to make connection to website." << std::endl;
-		return 0;   
-	}
-	else
-	{
-		// Convert the port to the correct endianness
-		Uint8* port = reinterpret_cast<Uint8*>(&ip.port);
-
-		std::cout << "Port num in NBO is: " << ip.port << std::endl;
-		std::cout << "Port number in correct endian: " << static_cast<unsigned int>(port[1]) << std::endl;
-
-		Uint8* iphost = reinterpret_cast<Uint8*>(&ip.host);
-
-		std::cout << "The IPV4 address of the host is: " << static_cast<unsigned int>(iphost[0]) << ".";
-		std::cout << static_cast<unsigned int>(iphost[1]) << ".";
-		std::cout << static_cast<unsigned int>(iphost[2]) << ".";
-		std::cout << static_cast<unsigned int>(iphost[3]) << std::endl;
-	}
-
-	clientSocket = SDLNet_TCP_Open(&ip);
-
-	if (clientSocket == nullptr)
-	{
-		std::cout << "Could not open clientSocket" << std::endl;
 		return 0;
 	}
+		
+
+	// 
+	if (SDLNet_ResolveHost(&ip, nullptr, 1234) == -1)
+	{
+		std::cout << "Failed to create a server." << std::endl;
+		system("pause");
+		return 0;
+	}
+
+	// Establishing the server's listerner socket 
+	// to prepare for connection with a client
+	// This will happen after we assured that the 
+	// server is created without errors
+	listenSocket = SDLNet_TCP_Open(&ip);
+
+
+	if (listenSocket == nullptr)
+	{
+		std::cout << "Could not crete a server's listerner socket.\n" << std::endl;
+		system("pause");
+		return 0;
+	}
+
+	std::cout << "Server socket ready for connection...\n" << std::endl;
+	std::cout << "Awaiting connection..." << std::endl;
+
+
+	// If connection is possible, the client will
+	// attempt to accept the connection with the server
+	// every half of second
+	while (!clientSocket)
+	{
+		SDLNet_TCP_Accept(listenSocket);
+		std::cout << ".";
+		SDL_Delay(500);
+	}
+
+	SDLNet_TCP_Close(listenSocket);
+	std::cout << "Connection established with client." << std::endl;
+
+
+	message = "Hello and welcome to the chat app, est. 1989";
+
+	int length = message.length() + 1;
+
+	if (SDLNet_TCP_Send(clientSocket, message.c_str(), length) < length)
+	{
+		std::cout << "Message could not be sent. Possble loss of data.\n" << std::endl;
+	}
 	else
 	{
-		message = "GET /HTTPS/1.0\r\n\n\n";
-
-		int length = message.length() + 1;
-
-		if (SDLNet_TCP_Send(clientSocket, message.c_str(), length) < length)
-		{
-			std::cout << "Message could not be requested. Possibly too long." << std::endl;
-			return 0;
-		}
-		else
-		{
-			char response[2000];
-
-			if (SDLNet_TCP_Recv(clientSocket, response, 2000) <= 0)
-			{
-				std::cout << "Message not recieved. Possible Loss of data"
-					<< std::endl;
-				return 0;
-			}
-			else
-			{
-				std::cout << response << std::endl;
-			}
-		}
-
-		SDLNet_TCP_Close(clientSocket);
+		std::cout << "Message succesfully sent." << std::endl;
 	}
+
+
+	SDLNet_TCP_Close(clientSocket);
 
 	Shutdown();
 
